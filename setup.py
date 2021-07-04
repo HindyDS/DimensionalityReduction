726d356e3edfcb09cf7bcc9336f553b05b1dbd3b
Hindy <hindy713@gmail.com>
2021-07-04 12:17:03 +0800

    initial commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('C:/Users/Hindy/Desktop/Container_DimensionalityReduction/README.md', 
     
 setuptools.setup(
     name='DimensionalityReduction', 
-    version='1.1.0',
+    version='1.1.1',
     author='Hindy Yuen', 
     author_email='hindy888@hotmail.com',
     license='MIT',
